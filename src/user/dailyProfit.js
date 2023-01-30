a3ecdb58d9afb3837e0fe05842bba89a8ebb7705
upgrace-in <thedesiretree47@gmail.com>
2023-01-31 00:00:12 +0530

    work

--- src/user/dailyProfit.js
+++ src/user/dailyProfit.js
@@ -7,53 +7,56 @@ async function dailyProfit() {
         const users = await Users.find()
         for (var i = 0; i < users.length; i++) {
 
-            let finalIncome = 0
-            let plans = users[i].plans
+            if (users[i].userID !== 'admin') {
 
-            // Loop through all the plans a user has (put the amt invested in a variable)
-            for (var j = 0; j < plans.length; j++) {
+                let finalIncome = 0
+                let plans = users[i].plans
 
-                // Fetch the plan details (dailyProfitPercent)
-                const plan = await db.collection('plans').findOne({ planID: parseInt(plans[j].planID) })
-                let dailyProfitPercent = parseFloat(plan.dailyProfit)
+                // Loop through all the plans a user has (put the amt invested in a variable)
+                for (var j = 0; j < plans.length; j++) {
 
-                // Invested amt per plan
-                let investAmt = parseFloat(plans[j].amount)
+                    // Fetch the plan details (dailyProfitPercent)
+                    const plan = await db.collection('plans').findOne({ planID: parseInt(plans[j].planID) })
+                    let dailyProfitPercent = parseFloat(plan.dailyProfit)
 
-                // Fetch Calculate the income (Add the income to the finalIncome)
-                finalIncome = finalIncome + (parseFloat(investAmt) * parseFloat(dailyProfitPercent)) / 100
-            }
-            // Update to the DB
-            // Fetch the user's old dailylevelincome
-            const oldIncome = await Incomes.findOne({ userID: users[i].userID });
+                    // Invested amt per plan
+                    let investAmt = parseFloat(plans[j].amount)
 
-            if ((oldIncome !== null) && (oldIncome.dailyProfit !== undefined))
-                // Adding the old income to the new one
-                finalIncome = parseFloat(finalIncome) + parseFloat(oldIncome.dailyProfit)
+                    // Fetch Calculate the income (Add the income to the finalIncome)
+                    finalIncome = finalIncome + (parseFloat(investAmt) * parseFloat(dailyProfitPercent)) / 100
+                }
+                // Update to the DB
+                // Fetch the user's old dailylevelincome
+                const oldIncome = await Incomes.findOne({ userID: users[i].userID });
 
-            await updateIncome(users[i].userID, { dailyProfit: finalIncome }).then(async (val) => {
-                if (val)
-                    // fethching the days
-                    await DailyprofitLogs.findOne({ userID: users[i].userID }).then(async (val, err) => {
-                        if (val)
-                            // updating the dailyprofit logs 
-                            days = parseInt(val.days)
-                        else
-                            days = 0
-                        await DailyprofitLogs.insertMany(
-                            {
-                                userID: users[i].userID,
-                                days: days + 1,
-                                amount: finalIncome
-                            }
-                        ).then(async (val, err) => {
-                            if (val === false)
-                                throw users[i].userID
+                if ((oldIncome !== null) && (oldIncome.dailyProfit !== undefined))
+                    // Adding the old income to the new one
+                    finalIncome = parseFloat(finalIncome) + parseFloat(oldIncome.dailyProfit)
+
+                await updateIncome(users[i].userID, { dailyProfit: finalIncome }).then(async (val) => {
+                    if (val)
+                        // fethching the days
+                        await DailyprofitLogs.findOne({ userID: users[i].userID }).then(async (val, err) => {
+                            if (val)
+                                // updating the dailyprofit logs 
+                                days = parseInt(val.days)
+                            else
+                                days = 0
+                            await DailyprofitLogs.insertMany(
+                                {
+                                    userID: users[i].userID,
+                                    days: days + 1,
+                                    amount: finalIncome
+                                }
+                            ).then(async (val, err) => {
+                                if (val === false)
+                                    throw users[i].userID
+                            })
                         })
-                    })
-                else
-                    throw users[i].userID
-            });
+                    else
+                        throw users[i].userID
+                });
+            }
         }
         return { msg: true }
     } catch (e) {
